78e7fb529fd82484f239cfaef88ee2d8160d778a
Cleiton <cleiton.siqueira.266@gmail.com>
2023-02-21 10:18:51 -0300

    Update

--- src/app/teachers/teachers.component.ts
+++ src/app/teachers/teachers.component.ts
@@ -9,13 +9,13 @@ export class TeachersComponent {
   title: string = 'Professores';
 
   public teachers = [
-    { name: 'Roberto' },
-    { name: 'Claudia' },
-    { name: 'Rosângela' },
-    { name: 'Cirlene' },
-    { name: 'Otto' },
-    { name: 'Heitor' },
-    { name: 'Vânia' },
+    { id: 1, name: 'Roberto', subject: 'Matemética' },
+    { id: 2, name: 'Claudia', subject: 'Física' },
+    { id: 3, name: 'Rosângela', subject: 'Química' },
+    { id: 4, name: 'Cirlene', subject: 'Português' },
+    { id: 5, name: 'Otto', subject: 'Artes' },
+    { id: 6, name: 'Heitor', subject: 'Educação Física' },
+    { id: 7, name: 'Vânia', subject: 'História' },
   ]
 
 }
